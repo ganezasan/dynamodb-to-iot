d3cf84ea6793ba82d215cc28f9865d2d750415a1
ganezasan <istaka12@gmail.com>
2017-10-11 13:37:08 +0900

    fix(sendMessageToIot): fix publish message

--- functions/sendMessageToIot/handler.js
+++ functions/sendMessageToIot/handler.js
@@ -1,1 +1,16 @@
-module.exports.handler = (event, context, callback) => callback(null, 'Hello');
+const AWS = require('aws-sdk');
+const iotData = new AWS.IotData({ endpoint: process.env.AWS_IOT_ENDPOINT });
+
+const handler = (event, context) => {
+  const params = {
+    topic: process.env.AWS_IOT_TOPIC,
+    payload: JSON.stringify(event)
+  }
+
+  iotData.publish(params, (err, res) => {
+    if (err) return context.fail(err);
+    return context.succeed();
+  });
+};
+
+exports.handler = handler;
